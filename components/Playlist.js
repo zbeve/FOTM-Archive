ea6eb24f8c01dbdaddb96231b0207aaed532f4f6
Zack Beveridge <zackbeveridge55@gmail.com>
2022-02-17 19:22:11 -0800

    a bit of styling, and horizontal scroll on mobile

--- components/Playlist.js
+++ components/Playlist.js
@@ -1,46 +1,37 @@
 import Image from 'next/image'
-import { getFollowers } from '../lib/spotify'
-import { useCallback, useEffect, useState } from 'react'
 import Link from 'next/link'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMusic, faPerson } from '@fortawesome/free-solid-svg-icons'
 
 import styles from '../styles/Playlist.module.scss'
 
 function Playlist({ playlist }) {
-    // const [data, setData] = useState(null)
-
-    // const fetchData = useCallback(async () => {
-    //     const response = await getFollowers(playlist.id)
-    //     const { items } = await response.json()
-    //     setData(items)
-    // }, [])
-
-    // useEffect(() => {
-    //     fetchData()
-    // }, [fetchData])
-
-    // console.log(playlist)
   return (
-    <Link as={`/playlist/${playlist.id}`} href="/playlist/[slug]">
-        <div className={ styles.container }>
-            <div className={ styles.image }>
-                <Image layout="responsive" src={ playlist.image.src } width="640" height="640" />
-            </div>
-            <div className={ styles.info }>
-                <h2 className={ styles.name }>{ playlist.name }</h2>
-                <div className={ styles.count }>
-                    <p className={ styles.songCount }>
-                        { playlist.tracks.total }
-                    </p>
-                    <p>
-                        {playlist.followers}
-                    </p>
-                    <p>
-                        {/* {playlist.tracks} */}
-                    </p>
-                </div>
+    <div className={ styles.container }>
+        <div className={ styles.image }>
+            <Image layout="responsive" src={ playlist.image.src } width="640" height="640" />
+        </div>
+        <div className={ styles.info }>
+            <h2 className={ styles.name }>{ playlist.name }</h2>
+
+            <div className={ styles.buttons }>
+                <Link as={`/playlist/${playlist.id}`} href="/playlist/[slug]">
+                    <button className={ styles.button }>Stats</button>
+                </Link>
+                <Link href={ playlist.url }>
+                    <a target="_blank" className={ styles.button }>Open In Spotify</a>
+                    {/* <button target="_blank" className={ styles.button }>Open In Spotify</button> */}
+                </Link>
             </div>
         </div>
-    </Link>
+        <div className={ styles.count }>
+            <FontAwesomeIcon className={ styles.icon } icon={ faMusic } />
+            <p className={ styles.songCount }>    
+                { playlist.tracks.count }
+            </p>
+        </div>
+    </div>
   )
 }
 
