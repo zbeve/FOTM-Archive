9bcbe8e60c5d10655709678ddb188a9baa6f8c03
Zack Beveridge <zackbeveridge55@gmail.com>
2022-02-18 16:36:48 -0800

    playlist page descriptions, styling, and stats

--- pages/playlist/[slug].js
+++ pages/playlist/[slug].js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { usePlaylistContext } from '../../components/PlaylistContext'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMusic, faPerson, faHouse, faChevronRight, faChevronLeft, faHourglassClock, faVolumeHigh } from '@fortawesome/free-solid-svg-icons'
+import { faMusic, faPerson, faHouse, faChevronRight, faChevronLeft, faHourglass, faVolumeHigh } from '@fortawesome/free-solid-svg-icons'
 
 import styles from '../../styles/Slug.module.scss'
 import StatsBar from '../../components/StatsBar'
@@ -14,21 +14,53 @@ import Link from 'next/link'
 export default function Playlist({ playlist, songData, nextSlug, prevSlug }) {
   const router = useRouter()
 
+  const explanations = [
+    {
+      name: "Introduction",
+      description: "The statistics above are calculated using Spotify's web API. How these numbers are exactly calculated is unknown (to me, at least), but the following descriptions are translated from their documentation. These are decided on a track by track basis. To calculate metrics for an entire playlist, these are processed for every track within a given playlist and averaged (this may lead to most playlists having similar metrics, and those with less tracks being more skewed). Obviously this introduces a bit of bias, but nonetheless fun insight into playlist 'feeling'. I suppose gates could be implemented to show larger variation in the data over the given set, but oh well. Enjoy",
+    },
+    {
+      name: "Danceability",
+      description: "Danceability describes how suitable a playlist is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0 is least danceable and 100 is most danceable.",
+    },
+    {
+      name: "Energy",
+      description: "Energy is a measure from 0 to 100 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy.",
+    },
+    {
+      name: "Valence",
+      description: "A measure from 0 to 100 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry).",
+    },
+    {
+      name: "Acousticness",
+      description: "A confidence measure from 0 to 100 of whether the playlist's tracks are acoustic. 100% represents high confidence the playlist is acoustic.",
+    },
+    {
+      name: "Instrumentalness",
+      description: "Predicts whether a track contains no vocals. 'Ooh' and 'aah' sounds are treated as instrumental in this context. Rap or spoken word tracks are clearly 'vocal'. The closer the instrumentalness value is to 100, the greater likelihood the track contains no vocal content. Values above 50 are intended to represent instrumental tracks, but confidence is higher as the value approaches 100.",
+    },
+    {
+      name: "Liveness",
+      description: "Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 80 provides strong likelihood that the track is live.",
+    },
+    {
+      name: "Speechiness",
+      description: "Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 100 the attribute value. Values above 66 describe tracks that are probably made entirely of spoken words. Values between 33 and 66 describe tracks that may contain both music and speech, either in sections or layered, including such cases as rap music. Values below 33 most likely represent music and other non-speech-like tracks.",
+    },
+    {
+      name: "Tempo",
+      description: "(represented by the hourglass icon) The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration.",
+    },
+    {
+      name: "Loudness",
+      description: "(represented by the volume icon) The overall loudness of a track in decibels (dB). Loudness values are averaged across the entire track and are useful for comparing relative loudness of tracks. Loudness is the quality of a sound that is the primary psychological correlate of physical strength (amplitude). Values typically range between -60 and 0 db.",
+    },
+
+  ]
+
   if (!router.isFallback && !playlist?.id) {
     return <ErrorPage statusCode={404} />
   }
-
-  // const playlistList = usePlaylistContext()
-  // let currentIndex = 0
-  
-  // playlistList.playlistList.find((el, index) => {
-  //   if(el.id == playlist.id) {
-  //     currentIndex = index
-  //     return true
-  //   }
-  // })
-
-  // console.log(playlistList.playlistList[currentIndex].id)
 
   return (
     <>
@@ -63,14 +95,6 @@ export default function Playlist({ playlist, songData, nextSlug, prevSlug }) {
             </div>
 
             <div className={ styles.container }>
-              {/* { console.log(playlist) }
-              { console.log("-----------------") }
-              { console.log(songs) }
-              { console.log("-----------------") }
-              { console.log(contributors )}
-              { console.log("-----------------") }
-              { console.log(averages )} */}
-
               <div className={ styles.image }>
                 <Image layout="responsive" src={ playlist.images[0].url } width="640" height="640" />
               </div>
@@ -78,18 +102,35 @@ export default function Playlist({ playlist, songData, nextSlug, prevSlug }) {
               <div className={ styles.stats }>
                 <h1 className={ styles.name }>{ playlist.name }</h1>
                 <div className={ styles.counts }>
-                  <div className={ styles.wrapper }>
-                    <FontAwesomeIcon icon={ faMusic } />
-                    <p className={ styles.song }>{ playlist.tracks.total }</p>
-                  </div>
                   <div className={ styles.wrapper}>
                     <FontAwesomeIcon icon={ faPerson } />
-                    <p className={ styles.followers }>{ playlist.followers.total }</p>
+                    <p className={ styles.text }>{ playlist.followers.total }</p>
+                  </div>
+                  <div className={ styles.wrapper }>
+                    <FontAwesomeIcon icon={ faMusic } />
+                    <p className={ styles.text }>{ playlist.tracks.total }</p>
+                  </div>
+                  <div className={ styles.wrapper}>
+                    <FontAwesomeIcon icon={ faHourglass } />
+                    <p className={ styles.text }>{ songData.tempo } bpm</p>
+                  </div>
+                  <div className={ styles.wrapper}>
+                    <FontAwesomeIcon icon={ faVolumeHigh } />
+                    <p className={ styles.text }>{ songData.loudness } db</p>
                   </div>
                 </div>
                 <div className={ styles.stats }>
                   { Object.keys(songData.averages).map((key, value) => (
                     <StatsBar key={ key.id } name={ key } value={ parseFloat(songData.averages[key]) }/>
+                  ))}
+                </div>
+                <div className={ styles.statsExplained }>
+                  { explanations.map(explanation => (
+                    <div className={ styles.explanation }>
+                      <hr></hr>
+                      <h2 className={ styles.name }>{ explanation.name }</h2>
+                      <p className={ styles.description }>{ explanation.description }</p>
+                    </div>
                   ))}
                 </div>
               </div>
